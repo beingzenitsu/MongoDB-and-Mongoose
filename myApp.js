c09cbca89fcb76e1cde8e4ba1035f5855d63513c
beingzenitsu <ojasvchaudhary0@gmail.com>
2025-08-21 15:45:13 +0530

    url mongoose

--- myApp.js
+++ myApp.js
@@ -2,7 +2,11 @@ require("dotenv").config();
 
 const  mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
 let Person;
 
 const createAndSavePerson = (done) => {
