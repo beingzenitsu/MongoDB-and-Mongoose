efce743941e2aad3fe4d303f575b9259e8569884
beingzenitsu <ojasvchaudhary0@gmail.com>
2025-08-25 15:19:31 +0530

    mongodb connection

--- myApp.js
+++ myApp.js
@@ -5,6 +5,8 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
 
 let Person;
 
